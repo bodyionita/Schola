b7acb1a90ab65c879ed2bb067d117bdad981a122
Bogdan Ionita <bogdan.ionita96@gmail.com>
2017-03-30 01:03:01 +0100

    Revert to initial

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="assets/js/jquery-3.1.1.min.js"></script>
-<script src="assets/js/bootstrap.min.js"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
-</footer>
--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-    require_once("redirect.php");
-    redirect("index.php");
-?>
